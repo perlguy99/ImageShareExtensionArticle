9ed6952a2bbbfbca264db397d155293260394ff3
Brent Michalski <Brent.Michalski@gmail.com>
2024-07-10 14:34:12 -0500

    Initial Commit

---
+++ ImageShareExtensionArticle/ImageShareExtensionArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageShareExtensionArticleApp.swift
+//  ImageShareExtensionArticle
+//
+//  Created by Brent Michalski on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageShareExtensionArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
